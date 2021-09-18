50ef3f7fad1f5ac06252ed233f4d12b792001e38
nwkami5010 <715190184@qq.com>
2021-09-18 23:01:45 +0800

    展示logo

--- pages/hot/hot.vue
+++ pages/hot/hot.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="hot-container">
-		<image class="logo" src="@/static/images/logo.png" ></image>
+		<image class="logo" src="@/static/images/logo.png" mode="aspectFit" ></image>
 	</view>
 </template>
 
@@ -15,5 +15,26 @@
 </script>
 
 <style lang="scss">
-
+.hot-container {
+  background-color: $uni-bg-color;
+  .logo {
+    width: 100%;
+    height: 40px;
+  }
+  .search-box {
+    padding: 0 16px;
+    margin-bottom: $uni-spacing-col-base;
+  }
+  .tab-sticky {
+    position: -webkit-sticky;
+    position: sticky;
+    z-index: 99;
+    /* #ifndef H5 */
+    top: 0;
+    /* #endif */
+    /* #ifdef H5 */
+    top: 44px;
+    /* #endif */
+  }
+}
 </style>
