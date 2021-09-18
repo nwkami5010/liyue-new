426a6d0bc76238c1370dba7267be11a9ef8a5802
nwkami5010 <715190184@qq.com>
2021-09-18 22:57:31 +0800

    Initial commit

---
+++ pages/hot/hot.vue
@@ -0,0 +1,19 @@
+<template>
+	<view class="hot-container">
+		<image class="logo" src="@/static/images/logo.png" ></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
